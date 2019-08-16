c04411bb9e38cd828c8185210ff2007d9cfc2398
Tony Rein <tonyrein@users.noreply.github.com>
2019-08-16 17:44:32 -0400

    Added jpg and jpeg to extensions list

--- make_base64_img_link.py
+++ make_base64_img_link.py
@@ -5,11 +5,8 @@ Create base64 links from image files, suitable for
 embedding in an html file.
 
 Takes a directory name as an argument. For each
-PNG file, creates a
-text file called <infile name>_base64.txt, containing
-an img tag suitable for pasting into html code.
-
-Copyright 2019 by Tony Rein
+image file (.jpg, .jpeg, .png), creates a
+text file called <infile name>_base64.txt
 
 uses:
 base64
@@ -20,15 +17,29 @@ import base64
 from pathlib import Path 
 import sys
 
+"""
+Get a list of pathlib.Path objects
+for files with the correct extensions.
+Check for extension is case-insensitive (that is,
+".PNG" is the same as ".png")
+"""
 def get_image_file_list(dirpath):
     hits = []
-    suffixes = [ '.png' ]
+    suffixes = [ '.jpg', '.jpeg', '.png' ]
     for suffix in suffixes:
         for subpath in dirpath.glob('*'):
             ext = subpath.suffix
             if ext.lower() == suffix:
                 hits.append(dirpath / subpath.name)
     return hits
+
+def get_file_type(infile):
+    ext = infile.suffix.lower()
+    if ext.startswith('.'):
+        ext = ext[1:]
+    if ext == 'jpeg':
+        ext = 'jpg'
+    return ext
 
 def make_outfile_name(infile):
     return f"{infile}_base64.txt"
@@ -38,18 +49,20 @@ def make_encoded_string(infile):
         encoded_string = base64.b64encode(image_file.read())
     return encoded_string
 
-def write_to_outfile(outfile_name, encoded_string):
-    stem = '<img src="data:image/png;base64,'.encode('utf-8')
+def write_to_outfile(file_type, outfile_name, encoded_string):
+    stem_str = f'<img src="data:image/{file_type};charset=utf-8;base64,'
+    stem = stem_str.encode('utf-8')
     tail = '">'.encode('utf-8')
     bytes_to_write = stem + encoded_string + tail
     with open(outfile_name, "wb") as f:
         f.write(bytes_to_write)
 
 def make_outfile(infile):
+    file_type = get_file_type(infile)
     outfile_name = make_outfile_name(infile)
     print(f"Processing {outfile_name}...")
     encoded_string = make_encoded_string(infile)
-    write_to_outfile(outfile_name, encoded_string)
+    write_to_outfile(file_type, outfile_name, encoded_string)
 
 def make_outfiles(infiles):
     outfile_count = 0
